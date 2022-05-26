9a8c2176c7f1dc724f10d445469c47db8ce4e189
Arch0wl <annstaroviotov@gmail.com>
2022-05-26 11:06:34 -0400

    New style was added

--- App.js
+++ App.js
@@ -1,20 +1,45 @@
+import { useEffect, useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, View, ActivityIndicator, ImageBackground, ScrollView } from 'react-native';
+import RestaurantCard from './src/components/RestaurantCard';
+import styles from './src/styles';
+
+const bgImage = {uri: 'https://png.pngtree.com/background/20210709/original/pngtree-food-western-food-steak-tomato-picture-image_941801.jpg'}
 
 export default function App() {
+  const [allRestaurants, setAllRestaurants] = useState()
+
+  useEffect( async () => {
+    const getData = async () => {
+      try {
+        const response = await fetch('https://my-first-firestore-hs.web.app/restaurants')
+        const data = await response.json()
+        setAllRestaurants(data)
+      } catch(err) {
+        console.error(err)
+      }
+    };
+    
+    getData()
+   }, []) 
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <ImageBackground resizeMode='cover' source={bgImage} style={styles.container}>
+      <SafeAreaView style={styles.container}>
+        <ScrollView>
+          {!allRestaurants
+            ? <ActivityIndicator size='large' color='orange' />
+            :
+            allRestaurants.map(singleRest => (
+              <RestaurantCard key={singleRest.id} singleRest={singleRest} />
+            ))
+          }
+        </ScrollView>
+      </SafeAreaView>
       <StatusBar style="auto" />
-    </View>
-  );
+    </ImageBackground>
+  </View>
+);
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    
---
+++ src/styles/index.js
@@ -0,0 +1,42 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+container: {
+        marginVertical: 20,
+        flex: 1,
+        width: '100%',
+        backgroundColor: '#00000035',
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginHorizontal: 0,
+      },
+      restaurantCard: {
+        wisth: '100%',
+        marginVertical: 16,
+        backgroundColor: '#fff',
+        color: '#444',
+        borderRadius: 6,
+      },
+      restaurantsName: {
+        marginHorizontal: 12,
+        fontSize: 24,
+        fontWeight: '600',
+        marginVertical: 15,
+      },
+
+      cuisine: {
+        marginHorizontal: 12,
+        fontSize: 16,
+        fontWeight: '100',
+        color: '#ff3333',
+      },
+
+      address: {
+        marginHorizontal: 12,
+        fontSize: 14,
+        fontWeight: '300',
+        color: '#303030',
+        marginBottom: 8,
+      },
+
+});
